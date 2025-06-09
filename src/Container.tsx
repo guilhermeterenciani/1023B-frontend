6e88f5c1a07043a8f7122f1b99a085851578e39e
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-06-09 12:39:38 -0300

    Exemplo de mudar valores de componentes sem utilizar useState.

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,40 +1,17 @@
-//Props -> é utilizado para passar um 
-// dados diferente para cada 
-// componente em sua criação.
-
-//States -> Estados, salvam estados de variáveis
-// do react e quando esses estados mudam.
-// Automaticamente o React Entende que ele 
-// precisa atualizar a página para mostrar
-// os novos valores do componente.
-//Class
-import React from 'react'
-interface ContainerProps{
+interface ContainerProps {
   nome:string
 }
-interface ContainerInfoState{
-  nome:string
-}
-
-//Container é filha de React.Component
-class Container extends React.Component<ContainerProps,ContainerInfoState>{
-  constructor(props:ContainerProps){
-    super(props)
-    this.state = {
-      nome:"Nome antes de Mudar"
-    }
+function Container(props:ContainerProps){
+  let texto = "Texto Antes de Mudar"
+  function mudar(){
+    texto = "Texto Depois de Mudar"
   }
-  mudar = ()=>{
-    this.setState({nome:"Nome depois de Mudar"})
-  }
-  render(): React.ReactNode {
-    return (
-      <>
-        <h1>{this.props.nome}</h1>
-        <p>{this.state.nome}</p>
-        <button onClick={this.mudar}>Mudar Estado</button>
-      </>
-    )
-  }
+  return(
+    <>
+      <h1>{props.nome}</h1>
+      <p>{texto}</p>
+      <button onClick={mudar}>Mudar</button>
+    </>
+  )
 }
 export default Container

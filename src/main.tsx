aadedd4243d10a907c9c82229bc9918ad936d2e0
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-06-10 09:18:41 -0300

    Introdução do Header css

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
+import './main.css'
+import Header from './Header.tsx'
 import Container from './Container.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Container nome='Header'/>
+    <Header/>
     <Container nome='Body'/>
   </StrictMode>,
 )
